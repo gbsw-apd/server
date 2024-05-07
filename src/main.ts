e46db9def0732fc25842228983c18ad44b67c543
wlstmd <jinwoo8288@gmail.com>
2024-05-07 16:27:24 +0900

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3030;
-
+  const config = new DocumentBuilder() //
+    .setTitle('API')
+    .setDescription('개발을 위한 API 문서입니다.')
+    .setVersion('1.0')
+    .addBasicAuth()
+    .addSecurity('basic', {
+      type: 'http',
+      scheme: 'basic',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(port);
-  console.log(`http://localhost:${port}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
+  console.log(`Listening on port ${port}`);
 }
 bootstrap();
